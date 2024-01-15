194768dbdc196c14a0b5a0ee54cc9dd0e41f41f4
pako8101 <112328223+pako8101@users.noreply.github.com>
2024-01-15 11:45:19 +0200

    Initial commit

---
+++ src/main/java/com/resellerapp/PureFutureApp.java
@@ -0,0 +1,12 @@
+package com.resellerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureFutureApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureFutureApp.class, args);
+    }
+}
